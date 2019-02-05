6d1b1f11dd952af88f18ea2ee504d3210c9b992f
fyang1024 <fyang1024@gmail.com>
2019-02-05 23:22:46 +1100

    removed theme provider

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Page from './Page';
 import * as serviceWorker from './serviceWorker';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Provider as StoreProvider } from 'react-redux'
 import store from './store';
 
-const muiTheme = getMuiTheme();
-
 function App(){
   return <StoreProvider store={store}>
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <Page />
-      </MuiThemeProvider>
+    <Page />
   </StoreProvider>;
 }
 
